271b88b17b2974e1e79d0c0ec75bd1666dd0f8a6
vosslerbr <vosslerbrady@gmail.com>
2020-09-15 13:42:33 -0500

    began front end, can now show weather data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/sartell`).then((res) => {
+      console.log(res.data.currentWeatherData);
+      const data = res.data.currentWeatherData;
+      this.setState({ data });
+    });
+  }
+
+  render() {
+    return <p>{this.state.data.feels_like}</p>;
+  }
 }
-
-export default App;
