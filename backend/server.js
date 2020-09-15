30048918fef6de9fa42ba116d6676ad4e7561eeb
vosslerbr <vosslerbrady@gmail.com>
2020-09-14 19:18:58 -0500

    finished MapBox geocoding integration

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/:lat&:long", (req, res) => {
-  const lat = req.params.lat;
-  const long = req.params.long;
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${process.env.WEATHER_API_KEY}&units=imperial`;
-  request(url, (error, response, body) => {
-    res.json(JSON.parse(body));
-  });
+app.get("/:citySearch", (req, res) => {
+  const cityName = req.params.citySearch;
 
-  const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/sartell.json?types=place&access_token=${process.env.MAPBOX_API_KEY}`;
-  request(mapUrl, (error, response, body) => {
-    console.log(body);
+  const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?types=place&access_token=${process.env.MAPBOX_API_KEY}`;
+  request(mapUrl, (error, response, mapBody) => {
+    const mapData = JSON.parse(mapBody);
+    const searchCityLat = mapData.features[0].center[1];
+    const searchCityLong = mapData.features[0].center[0];
+    console.log(`lat: ${searchCityLat}, long: ${searchCityLong}`);
+
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${searchCityLat}&lon=${searchCityLong}&appid=${process.env.WEATHER_API_KEY}&units=imperial`;
+    request(url, (err, resp, weatherBody) => {
+      res.json(JSON.parse(weatherBody));
+    });
   });
 });
 
